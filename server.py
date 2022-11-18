308e6588e4a5ae17a84ba75c842c1dbc91fd6cbc
Oleg <ya.slarej@ya.ru>
2022-11-18 15:39:13 +0300

    First atleast working version

--- server.py
+++ server.py
@@ -7,18 +7,29 @@ HDRS = 'HTTP/1.1 200 0K\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        server.bind(('127.0.0.1', 2000))
+        server.bind(('127.0.0.1', 2003))
         server.listen(10)
         while True:
             client_socket, adress = server.accept()
             print('Working...')
             data = client_socket.recv(1024).decode('utf-8')
             print(data)
-            if data.split()[0] == "GET":
+            if data.split()[1] == "/" or data.split()[1] == "/favicon.ico":
                 content = load_page_from_get_request(data)
-                # client_socket.send(content)
-                client_socket.send(b'HTTP/1.1 301 Moved Permanently\nLocation: https://www.google.com/')
+                client_socket.send(content)
+                # client_socket.send(b'HTTP/1.1 301 Moved Permanently\nLocation: https://www.google.com/')
                 client_socket.shutdown(socket.SHUT_WR)
+            elif data.split()[1][1] == "?":
+                data = data.split()[1]
+                link = data[7:]
+                url = tiny(link)
+                client_socket.send(HDRS.encode("utf-8") + f"127.0.0.1:2003/{url}".encode("utf-8"))
+            else:
+                data = data.split()[1][1:]
+                url = long(data)
+                client_socket.send(f'HTTP/1.1 301 Moved Permanently\nLocation: https://www.{url}/'.encode('utf-8'))
+
+
     except KeyboardInterrupt:
         server.close()
         print("Closing...")
@@ -27,13 +38,9 @@ def start_server():
 def load_page_from_get_request(request_data):
     path = request_data.split(' ')
     if len(path) > 1:
-        path = path[1].strip('/')
-        if path == "":
-            with open("views/home.html", "rb") as file:
-                response = file.read()
-            return HDRS.encode("utf-8") + response
-        print(long(path))
-        return HDRS.encode("utf-8")
+        with open("views/home.html", "rb") as file:
+            response = file.read()
+        return HDRS.encode("utf-8") + response
     else:
         return HDRS.encode("utf-8")
 
